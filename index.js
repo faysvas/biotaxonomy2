8aa23ee4671bbdaa265501a2c38a7c62d442b612
Golfo Vasiliou <faysvas@yahoo.com>
2020-03-22 18:18:44 +0200

    Set up react

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return <div>Simple Resfajhct App!</div>
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
